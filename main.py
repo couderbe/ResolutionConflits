9660c6dc249772a4248e600553288dda9f5c71cc
Couderc Benoit <couderc.benoit@orange.fr>
2019-11-25 23:53:51 +0100

    Reorganisation, ajout des __repr__, contruction de la fonction Init avec generation aleatoire des xi

--- main.py
+++ main.py
@@ -3,48 +3,16 @@ from PyQt5.QtCore import QPoint
 
 import radarview
 import math
-
-T = 200
-
-class Flight():
-    def __init__(self,speed,trajectory):
-        self.speed = speed
-        self.trajectory = trajectory
-
-    # return 5 QPoints qui sont debut,fin et cassures de la trajectoire
-    # angles en radians
-    def pointTrajectory(self):
-        p0 = self.trajectory.pointDepart
-        p1 = p0 + QPoint(self.speed*self.trajectory.manoeuvre.t0*math.cos(self.trajectory.angle0),
-                         self.speed*self.trajectory.manoeuvre.t0*math.sin(self.trajectory.angle0))
-        p2 = p1 + QPoint(self.speed*self.trajectory.manoeuvre.t1*math.cos(self.trajectory.manoeuvre.angle + self.trajectory.angle0),
-                         self.speed*self.trajectory.manoeuvre.t1*math.sin(self.trajectory.manoeuvre.angle + self.trajectory.angle0))
-        p3 = p2 + QPoint(self.speed*self.trajectory.manoeuvre.t1*math.cos(-self.trajectory.manoeuvre.angle + self.trajectory.angle0),
-                         self.speed*self.trajectory.manoeuvre.t1*math.sin(-self.trajectory.manoeuvre.angle + self.trajectory.angle0))
-        p4 = p3 + QPoint(self.speed*(T - self.trajectory.manoeuvre.t0 - 2*self.trajectory.manoeuvre.t1)*math.cos(self.trajectory.angle0),
-                         self.speed*(T - self.trajectory.manoeuvre.t0 - 2*self.trajectory.manoeuvre.t1)*math.sin(self.trajectory.angle0))
-        return [p0,p1,p2,p3,p4]
-
-class Manoeuvre():
-    def __init__(self,t0,angle,t1):
-        self.t0 = t0
-        self.angle = angle
-        self.t1 = t1
-
-class Trajectory():
-    def __init__(self,pointDepart, angle0, manoeuvre):
-        self.pointDepart = pointDepart
-        self.angle0 = angle0
-        self.manoeuvre = manoeuvre
-
+import pb
 
 
 if __name__ == "__main__":
     #trajectories = [[QPoint(x, y) for x in range(150)] for y in range(0, 300, 100)]
-    man = Manoeuvre(10, math.pi / 7, 15)
-    trajectoire = Trajectory(QPoint(0,0),math.pi/4,man)
-    vol1 = Flight(100,trajectoire)
-    trajectories = [vol1.pointTrajectory()]
+    man = pb.Manoeuvre(10, math.pi / 7, 15)
+    trajectoire = pb.Trajectory(QPoint(0,0),math.pi/4,man)
+    vol1 = pb.Flight(100,trajectoire)
+    #trajectories = [vol1.pointTrajectory()]
+    trajectories = [vol.pointTrajectory() for vol in pb.init()[0]]
     # Initialize Qt
     app = QtWidgets.QApplication([])
 
---
+++ pb.py
@@ -0,0 +1,64 @@
+from PyQt5.QtCore import QPoint
+import math
+import random
+
+T = 200 # Temps total
+N_avion = 10 # Nombre d'avion
+
+class Flight():
+    def __init__(self,speed,trajectory):
+        self.speed = speed
+        self.trajectory = trajectory
+
+    # return 5 QPoints qui sont debut,fin et cassures de la trajectoire
+    # angles en radians
+    def pointTrajectory(self):
+        p0 = self.trajectory.pointDepart
+        p1 = p0 + QPoint(self.speed*self.trajectory.manoeuvre.t0*math.cos(self.trajectory.angle0),
+                         self.speed*self.trajectory.manoeuvre.t0*math.sin(self.trajectory.angle0))
+        p2 = p1 + QPoint(self.speed*self.trajectory.manoeuvre.t1*math.cos(self.trajectory.manoeuvre.angle + self.trajectory.angle0),
+                         self.speed*self.trajectory.manoeuvre.t1*math.sin(self.trajectory.manoeuvre.angle + self.trajectory.angle0))
+        p3 = p2 + QPoint(self.speed*self.trajectory.manoeuvre.t1*math.cos(-self.trajectory.manoeuvre.angle + self.trajectory.angle0),
+                         self.speed*self.trajectory.manoeuvre.t1*math.sin(-self.trajectory.manoeuvre.angle + self.trajectory.angle0))
+        p4 = p3 + QPoint(self.speed*(T - self.trajectory.manoeuvre.t0 - 2*self.trajectory.manoeuvre.t1)*math.cos(self.trajectory.angle0),
+                         self.speed*(T - self.trajectory.manoeuvre.t0 - 2*self.trajectory.manoeuvre.t1)*math.sin(self.trajectory.angle0))
+        return [p0,p1,p2,p3,p4]
+
+    def __repr__(self):
+        return str(self.speed) + " " +self.trajectory
+
+class Manoeuvre():
+    def __init__(self,t0,t1,angle):
+        self.t0 = t0
+        self.angle = angle
+        self.t1 = t1
+
+    def __repr__(self):
+        return ("t0:" + str(self.t0) + " t1:" + str(self.t1) + " angle:" + str(self.angle))
+
+class Trajectory():
+    def __init__(self,pointDepart, angle0, manoeuvre):
+        self.pointDepart = pointDepart
+        self.angle0 = angle0
+        self.manoeuvre = manoeuvre
+
+    def __repr__(self):
+        return ("Depart:" + self.pointDepart + " manoeuvre:" + self.manoeuvre + " angle0:" + str(self.angle0))
+
+def calculConflit():
+    return None
+
+def init():
+    F = [Flight(100,Trajectory(QPoint(0,50*k),0.5*k,Manoeuvre(0,0,0))) for k in range(N_avion)]
+    X = []
+    x0 = [Manoeuvre(calculConflit(f0),0) for f0 in F]
+    X.append(x0)
+    for k in range(1,N_avion):
+        x = []
+        for l in range(N_avion):
+            angle = random.uniform(-math.pi/6,math.pi/6)
+            t0 = random.randint(0,200) # Les temps sont entier à voir
+            t1 = random.randint(t0,200)
+            x.append(Manoeuvre(t0,t1,angle))
+        X.append(x)
+    return F,X
