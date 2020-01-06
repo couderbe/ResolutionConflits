46e9051f83c25cab415056a71f6a57cbd78fe7f8
gourmede <alhena@caramail.fr>
2020-01-06 15:09:15 +0100

    On sauvegarde et reconstruit les résultats!!!!!!!!

---
+++ IO.py
@@ -0,0 +1,21 @@
+import numpy as np
+from PyQt5.QtCore import QPoint
+import testmain as pb
+
+###Fonction qui lit
+def read(filename):
+    Flights=[]
+    with open(filename, 'r' ) as file:
+        for line in file:
+            line2 = [float(elt) for elt in line.split(';')]
+            print(line2)
+            avion = pb.Flight(np.linalg.norm(np.array([line2[3], line2[4]])),QPoint(line2[0],line2[1]), line2[2], pb.Manoeuvre(line2[5], line2[6], line2[7]))
+            Flights.append(avion)
+    return Flights
+
+
+###Fonction qui écrit les résultats pour les sauvegarder
+def write(Flights, filename):
+    with open(filename, 'w') as file:
+        for f in Flights:
+            file.write('{0};{1};{2};{3[0]};{3[1]};{4.t0};{4.t1};{4.angle}\n'.format(f.pointDepart.x(),f.pointDepart.y(), f.angle0, f.speed, f.manoeuvre))
--- main.py
+++ main.py
@@ -7,44 +7,54 @@ import numpy as np
 from cmath import phase
 import testDE as de
 import time
-import random
+import IO
+import sys
 
 Flights = []
 m = 2*3.1416/pb.N_avion
-ITERATIONS = 100
+ITERATIONS = 15
 RAYON_CERCLE = 30000
+FICHIER = "Results/"
+
 
 if __name__ == "__main__":
+    if len(sys.argv)==1:
     #trajectories = [[QPoint(x, y) for x in range(150)] for y in range(0, 300, 100)]
     #man = pb.Manoeuvre(10,50,math.pi / 7)
     #trajectoire = pb.Trajectory(QPoint(0,0),math.pi/4,man)
     #vol1 = pb.Flight(100,trajectoire)
     #trajectories = [vol1.pointTrajectory()]
-    t = time.time()
-    v = [1 for k in range(pb.N_avion)] # avoir la vitesse des avions (différentes)
-    Flights = [pb.Flight(250, QPoint(v[k]*RAYON_CERCLE*np.cos(m*k), v[k]*RAYON_CERCLE*np.sin(m*k)),np.pi+phase(complex(v[k]*RAYON_CERCLE*np.cos(m*k),v[k]*RAYON_CERCLE*np.sin(m*k))), pb.Manoeuvre(pb.T, 0, 0)) for k in range(pb.N_avion)]
-    population = pb.creaPop(Flights)
-    solution = de.algoDE(pb.fitness,pb.BOUNDS,pb.N_pop, pb.F, pb.CR, ITERATIONS, population)
-    print("Temps d'exécution: " + str((time.time()-t)/60))
-    print("La meilleure solution est "+str(solution))
-    print(pb.fitness(solution))
+        t = time.time()
+        v = [1 for k in range(pb.N_avion)] # avoir la vitesse des avions (différentes)
+        Flights = [pb.Flight(250, QPoint(v[k]*RAYON_CERCLE*np.cos(m*k), v[k]*RAYON_CERCLE*np.sin(m*k)),np.pi+phase(complex(v[k]*RAYON_CERCLE*np.cos(m*k),v[k]*RAYON_CERCLE*np.sin(m*k))), pb.Manoeuvre(pb.T, 0, 0)) for k in range(pb.N_avion)]
+        population = pb.creaPop(Flights)
+        solution = de.algoDE(pb.fitness,pb.BOUNDS,pb.N_pop, pb.F, pb.CR, ITERATIONS, population)
+        print("Temps d'exécution: " + str((time.time()-t)/60))
+        print("La meilleure solution est "+str(solution))
+        print(pb.fitness(solution))
 ## Reconstruire une manoeuvre à partir d'un array (t0,theta, alpha):
 
-    for i,vol in enumerate(Flights):
+        for i,vol in enumerate(Flights):
         # manArray = np.array([0,0,0])
         # manArray[0] = solution[i][0]
         # manArray[1] = solution[i][1]
         # manArray[2] = solution[i][2]
-        vol.manoeuvre = pb.convertAtoM(solution[i])
-        print(vol.manoeuvre)
-    print(Flights)
+            vol.manoeuvre = pb.convertAtoM(solution[i])
+            print(vol.manoeuvre)
+        print(Flights)
+        date = time.ctime(time.time())
+        dateTest = "_".join(date.split())
+        filename = FICHIER + dateTest + '.txt'
+        IO.write(Flights, filename)
     #trajectories = [vol.pointTrajectory() for vol in Flights]
 
-
+    else:
+        Flights = IO.read(sys.argv[1])
     # Initialisation Qt
     app = QtWidgets.QApplication([])
 
     # create the radar view and the time navigation interface
+    #rad = radarview.RadarView(Flights)
     rad = radarview.RadarView(Flights)
     rad.move(10, 10)
 
