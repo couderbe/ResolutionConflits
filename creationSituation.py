1d367755a472a181aa98d9f50fa082c1ab8451fb
elhaouji <jihanne1409@hotmail.fr>
2020-01-10 17:19:08 +0100

    Les constantes ont changé

--- creationSituation.py
+++ creationSituation.py
@@ -14,32 +14,52 @@ def vitesseConstante (vitesse) :
 def vitesseAleatoire(vitesse) :
     return [random.uniform(vitesse[0],vitesse[1]) for k in range(pb.N_avion)]
 
-def cercle(m, RAYON_CERCLE, vitesse):
-    return [pb.Flight(vitesse[k], QPoint(RAYON_CERCLE * np.cos(m * k),  RAYON_CERCLE * np.sin(m * k)),
+def cercle(m, RAYON_CERCLE, liste_vitesse_avion):
+    return [pb.Flight(liste_vitesse_avion[k], QPoint(RAYON_CERCLE * np.cos(m * k),  RAYON_CERCLE * np.sin(m * k)),
                       np.pi + phase(complex( RAYON_CERCLE * np.cos(m * k), RAYON_CERCLE * np.sin(m * k))),
                       pb.Manoeuvre(pb.T, 0, 0)) for k in range(pb.N_avion)]
 
 
-def cercle_deforme(m, RAYON_CERCLE, vitesse, epsilon):
+def cercle_deforme(m, RAYON_CERCLE, liste_vitesse_avion, epsilon):
     deformation = [random.uniform(- epsilon, epsilon) for i in range(pb.N_avion)]
-    return [pb.Flight(vitesse[k], QPoint(RAYON_CERCLE * np.cos(m * k + deformation[k]),
+    return [pb.Flight(liste_vitesse_avion[k], QPoint(RAYON_CERCLE * np.cos(m * k + deformation[k]),
                                   RAYON_CERCLE * np.sin(m * k + deformation[k])), np.pi + phase(
         complex(RAYON_CERCLE * np.cos(m * k + deformation[k]),
                 RAYON_CERCLE * np.sin(m * k + deformation[k]))), pb.Manoeuvre(pb.T, 0, 0)) for k in
             range(pb.N_avion)]
 
 
-def hasard(vitesse):
-    L = []
-    i = 0
+def hasard(liste_vitesse_avion):
+    liste_angle = []
+    val_x = -100000 + 200000 * random.random()
+    val_y = -100000 + 200000 * random.random()
+    position = [np.array ([val_x, val_y])]
+    i = 1
     while i < pb.N_avion:
-        val_x = random.uniform(0, 10)
-        val_y = random.uniform(0, 10)
-        val = [val_x, val_y]
-        if val not in L:
-            L.append(val)
+        val_x = -100000 + 200000*random.random()
+        val_y = -100000 + 200000*random.random()
+        angle = random.uniform(-np.pi/6,np.pi/6)
+        val = np.array ([val_x, val_y])
+        distance = np.linalg.norm(position[-1] - val)
+        if distance >= pb.d :
+            position.append(val)
             i += 1
-
-    return [pb.Flight(vitesse, QPoint(L[k][0], L[k][1]), np.pi + complex(L[k][0], L[k][1]), pb.Manoeuvre(pb.T, 0, 0)) for k in range(pb.N_avion)]
-
+    #print(position)
+    for pos in position :
+        if pos[0] >= 0 : #val de x
+            if pos[1] >= 0 : #val de y"
+                angle_hasard = (3*np.pi)/4 + ((np.pi/4)*random.random())
+                liste_angle.append(angle_hasard)
+            else :
+                angle_hasard = np.pi/2 + ((np.pi/2) * random.random())
+                liste_angle.append(angle_hasard)
+        else:
+            if pos[1] >= 0:
+                angle_hasard = ((3 * np.pi) / 4)*random.random()
+                liste_angle.append(angle_hasard)
+            else :
+                angle_hasard = (np.pi/2) * random.random()
+                liste_angle.append(angle_hasard)
+    #print(liste_angle)
+    return [pb.Flight(liste_vitesse_avion[k], QPoint(position[k][0], position[k][1]), liste_angle[k], pb.Manoeuvre(pb.T, 0, 0)) for k in range(pb.N_avion)]
 
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 ###----------CONSTANTES--------###
 m = 2 * 3.1416 / pb.N_avion
-ITERATIONS = 10
+ITERATIONS = 200
 RAYON_CERCLE = 185200 # Rayon du cercle en mètres, correpondant à 100 NM
 FICHIER = "Results/"
 EPSILON = 0.8
@@ -20,8 +20,8 @@ VITESSE = 250
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         t = time.time()
-        v = cS.vitesseConstante(VITESSE)  # avoir la vitesse des avions (différentes si on choisit la situation aleatoire (mettre une liste de borne min et borne max)
-        Flights = cS.cercle(m,RAYON_CERCLE,v)
+        liste_vitesse_avion = cS.vitesseConstante(VITESSE)  # avoir la vitesse des avions (différentes si on choisit la situation aleatoire (mettre une liste de borne min et borne max)
+        Flights = cS.hasard (liste_vitesse_avion)
         population = pb.creaPop(Flights)
         solution,list_gen_avg,list_gen_best = de.algoDE(pb.fitness, pb.BOUNDS, pb.N_pop, pb.F, pb.CR, ITERATIONS, population)
 
@@ -35,7 +35,7 @@ if __name__ == "__main__":
         plt.legend(["Moyenne population","Meilleur individu"],loc = 4)
         plt.xlabel("Itérations")
         plt.ylabel("Fitness")
-        plt.show()
+        plt.show(block = False)
 
         print("Temps d'exécution: " + str((time.time() - t) / 60))
         print("La meilleure solution est " + str(solution))
