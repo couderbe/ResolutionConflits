621e67f849fb7772c4b6ff8a723bc43994896e11
couderbe <couderc.benoit@orange.fr>
2020-01-17 09:55:59 +0100

    Création fichier constantes et utilisation de la ligne de commande pour les modifiées

--- constantes.py
+++ constantes.py
@@ -7,26 +7,28 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-na", type=int, default=10, help="Nombre d'avions, initialisé à 10")
 parser.add_argument("-rc", type=int, default=185200, help="Rayon du Cercle de départ des avions")
-parser.add_argument("-rep", type=str, default="Results/")
-parser.add_argument("-rf", type=str, default="FitnessEvolution/")
+parser.add_argument("-rep", type=str, default="Results/", help="Répertoire d'enregistrement des résultats")
+parser.add_argument("-rf", type=str, default="FitnessEvolution/",
+                    help="Sous-répertoire d'enregistrement des scores de la fitness")
 parser.add_argument("-t", type=int, default=1800,
                     help="Temps total en secondes, initialisé à 30mn (correspondant à l'horizon visuel moyen d'un contrôleur en route)")
 parser.add_argument("-d", type=int, default=9260,
-                    help="Distance de séparation pour le conflit aérien, initialisé à 5NM (exprimé en m)")
+                    help="Distance de séparation pour le conflit aérien, initialisée à 5NM (exprimée en m)")
 parser.add_argument("-eps", type=float, default=0.8,
                     help="Paramètres de déformation du cercle de la configuration initiale d'avions")
 parser.add_argument("-v", default=250,
-                    help="Vitesse dans le cas d'avions ayant tous la même, couple de bornes Min/Max pour une répartition aléatoire des vitesses, initialisé à 250 m/s ")
+                    help="Vitesse dans le cas d'avions ayant tous la même, liste de bornes [Min,Max] pour une répartition aléatoire des vitesses, initialisé à 250 m/s ")
 parser.add_argument("-i", type=int, default=250,
                     help="Nombre d'itérations (générations) de l'algorithme ED, initialisé à 250")
 parser.add_argument("-np", type=int, default=40, help="Nombre d'individus dans la population, initialisé à 40")
-parser.add_argument("-cr", type=float, default=0.05, help="Paramètre CR (Change Rate) de l'ED, initialisé à 0.05")
+parser.add_argument("-cr", type=float, default=0.05, help="Probabilité CR (Change Rate) de l'ED, initialisée à 0.05")
 parser.add_argument("-f", type=float, default=0.7,
                     help="Paramètre F de l'ED, initialisé à 0.7. Doit être compris entre 0 et 2")
 parser.add_argument("-tf", type=int, default=0,
                     help="Configuration de départ des avions: 0:cercle  1:cercle déformé, 2:répartition pseudo-aléatoire ")
 parser.add_argument("-file", default=None, help="Permet de lire un fichier, d'une situation précédemment enregistrée")
-parser.add_argument("-st", type=int, default=0, help="Configuration des vitesses: 0:constantes 1:pseudo-aléatoire")
+parser.add_argument("-st", type=int, default=0,
+                    help="Configuration des vitesses: 0:constantes 1:pseudo-aléatoire (dans ce cas préciser obligatoirement une liste de bornes de vitesse)")
 args = parser.parse_args()
 
 """Constantes globales"""
